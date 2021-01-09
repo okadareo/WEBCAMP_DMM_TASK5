7eb6e09af9b3577a8fb13b8ba212bfa2b937f9a5
okadareo <okadareo0614@gmail.com>
2021-01-09 01:32:44 +0000

    Finish Task4

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -11,4 +11,15 @@ class RelationshipsController < ApplicationController
     current_user.unfollow(user)
     redirect_to request.referer
   end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
 end
